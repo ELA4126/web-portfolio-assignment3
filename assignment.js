313f8db8f51718a04147e056cf53cf0e22c9d5e7
ELA4126 <nnsiddika30@gmail.com>
2021-01-22 14:55:24 +0200

    finished my assignment

--- assignment.js
+++ assignment.js
@@ -1,3 +1,8 @@
+
+
+// 1. kilometerToMeter
+
+
 function kilometerToMeter (kiloMeter){
     var Meter = kiloMeter * 1000;
     return Meter;
@@ -8,7 +13,10 @@ console.log (distance);
 
 
 
-function budgetCalculator( watch ,phone, laptop) {
+// 2.budgetCalculator
+
+
+function budgetCalculator( watch , phone, laptop) {
 
     var watchCost = watch*50;
     var phoneCost = phone*100;
@@ -22,7 +30,48 @@ console.log( subtotalBudget);
 
 
 
+// 3. hotelCost
 
-function hotelCost
+
+function hotelCost(days){
+    var totalHotelCost = 0;
+    if(days <=10 ){
+        totalHotelCost = days * 100; 
+    }else if ( days <=20 ){
+        var firstHotelStayCost= 10 * 100;
+        var remainingDays = days - 10 ;
+        var secondHotelStayCost = remainingDays * 80 ;
+        totalHotelCost = firstHotelStayCost + secondHotelStayCost ;
+    }else {
+        var firstHotelStayCost = 10 * 100;
+        var secondHotelStayCost = 10 * 80 ;
+        var remainingDays = days - 20 ;
+        var thirdHotelStayCost = remainingDays * 50 ;
+        totalHotelCost = firstHotelStayCost + secondHotelStayCost + thirdHotelStayCost ;
+    }
+    return totalHotelCost;
+}
+var total = hotelCost (25);
+console.log ( total );
+
+
+
+// 4.megaFriend
+
+
+function megaFriend(names){
+    var longestName = names[0].length ;
+    var answar = names[0];
+    for (var i= 0 ; i <names.length; i++){
+        var magaName = names[i].length;
+        if (magaName > longestName){
+            answar = names[i];
+            longestName = magaName;
+        }
+    }
+    return answar;
+}
+var megaFriendName = megaFriend(["Rayaan", "Ela", "Mamun", "Abdullah", "Nahar"]);
+console.log (megaFriendName);
 
 
