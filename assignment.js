c535b245eec28c083da26d466dd16305f17533e9
ELA4126 <nnsiddika30@gmail.com>
2021-01-22 10:56:34 +0200

    modified my ripo

--- assignment.js
+++ assignment.js
@@ -1,1 +1,28 @@
-kilometertometer
+function kilometerToMeter (kiloMeter){
+    var Meter = kiloMeter * 1000;
+    return Meter;
+}
+var distance = kilometerToMeter(7);
+console.log (distance);
+
+
+
+
+function budgetCalculator( watch ,phone, laptop) {
+
+    var watchCost = watch*50;
+    var phoneCost = phone*100;
+    var laptopCost = laptop*500;
+
+    var totalBudget =  watchCost + phoneCost + laptopCost ;
+    return totalBudget;
+}
+var subtotalBudget = budgetCalculator(3 , 2, 1) ;
+console.log( subtotalBudget);
+
+
+
+
+function hotelCost
+
+
